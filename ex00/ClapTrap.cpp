bf73c8156323629058c46533234ca11a6f4fe40f
sboukiou <sboukiou@student.1337.ma>
2025-12-17 17:03:43 +0100

    Added Fixes and syntax improvment for parameters + More debuggin output + Destructor implemented

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,31 +1,40 @@
 #include "./ClapTrap.hpp"
 
-ClapTrap::ClapTrap(void) {
-	name = "";
-	hitPoints = 0;
-	energyPoints = 0;
-	attackDamage = 0;
-}
-
-ClapTrap::ClapTrap(std::string &param): hitPoints(0),
+ClapTrap::ClapTrap(void):
+	name("DEFAULT"),
+	hitPoints(0),
 	energyPoints(0),
 	attackDamage(0) {
-		name = param;
-}
+		std::cout << "[INFO]: (Default Constructor Called for " << getName() << ")\n";
+	}
 
-ClapTrap::ClapTrap(ClapTrap &other) {
+ClapTrap::ClapTrap(const std::string &param):
+	name(param),
+	hitPoints(0),
+	energyPoints(0),
+	attackDamage(0) {
+		std::cout << "[INFO]: (Parameterized Constructor Called for " << getName() << ")\n";
+		}
+
+ClapTrap::ClapTrap(const ClapTrap &other) {
 	name = other.getName();
 	hitPoints = other.getHitPoints();
 	energyPoints = other.getEnergyPoints();
 	attackDamage = other.getAttackDamage();
+		std::cout << "[INFO]: (Copy Constructor Called for " << getName() << ")\n";
 }
 
-ClapTrap	&ClapTrap::operator=(ClapTrap &other) {
+ClapTrap	&ClapTrap::operator=(const ClapTrap &other) {
 	name = other.getName();
 	hitPoints = other.getHitPoints();
 	energyPoints = other.getEnergyPoints();
 	attackDamage = other.getAttackDamage();
+	std::cout << "[INFO]: (Assignment operator Constructor Called for " << getName() << ")\n";
 	return (*this);
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << "[INFO]: (Destructor called for: " << getName() << ")\n";
 }
 
 void	ClapTrap::attack(const std::string &target) {
@@ -34,12 +43,12 @@ void	ClapTrap::attack(const std::string &target) {
 	energyPoints -= 1;
 }
 
-void	ClapTrap::takeDamage(unsigned int param) {
-	std::cout << "ClapTrap " << name <<  " was attacked  , and has lost  " << param << " points of damage!\n";
+void	ClapTrap::takeDamage(const unsigned int &param) {
+	std::cout << "ClapTrap " << name <<  " was attacked  , and has lost  " << param << " points!\n";
 	setEnergyPoints(getEnergyPoints() - param);
 }
 
-void	ClapTrap::beRepaired(unsigned int param) {
+void	ClapTrap::beRepaired(const unsigned int &param) {
 	if (energyPoints > 0)
 		std::cout << "ClapTrap " << name <<  " is repairing " << param << " points\n";
 	energyPoints -= 1;
@@ -50,7 +59,7 @@ unsigned int	ClapTrap::getHitPoints(void) const { return (hitPoints); }
 unsigned int	ClapTrap::getEnergyPoints(void) const { return (energyPoints); }
 unsigned int	ClapTrap::getAttackDamage(void) const { return (attackDamage); }
 
-void	ClapTrap::setName(std::string param) { name = param; }
-void	ClapTrap::setHitPoints(unsigned int param) { hitPoints = param; }
-void	ClapTrap::setEnergyPoints(unsigned int param) { energyPoints = param; }
-void	ClapTrap::setAttackDamage(unsigned int param) { attackDamage = param; }
+void	ClapTrap::setName(const std::string &param) { name = param; }
+void	ClapTrap::setHitPoints(const unsigned int &param) { hitPoints = param; }
+void	ClapTrap::setEnergyPoints(const unsigned int &param) { energyPoints = param; }
+void	ClapTrap::setAttackDamage(const unsigned int &param) { attackDamage = param; }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,29 +5,37 @@
 int main(void) {
 	std::cout <<  "------------------\n";
 	ClapTrap	User0;
-	std::string u1 = "User1";
 	std::cout <<  "------------------\n";
-	ClapTrap	User1(u1);
+	ClapTrap	User1("Dias");
 	std::cout <<  "------------------\n";
 	ClapTrap	User2(User0);
 	std::cout <<  "------------------\n";
-	ClapTrap	User3 = User1;
+	ClapTrap	User3(ClapTrap(ClapTrap("Test-User")));
 	std::cout <<  "------------------\n";
 
+	/*Setting the class properties*/
+	/*Manually*/
+	std::cout <<  "------------------\n";
+	std::cout << "Setters to setup the Object properties\n";
 	User0.setName("User 0");
 	User0.setHitPoints(10);
 	User0.setEnergyPoints(10);
 	User0.setAttackDamage(10);
+	std::cout <<  "------------------\n";
 
+	std::cout <<  "------------------\n";
 	User0.attack(" Enemy !");
+	std::cout <<  "------------------\n";
 	User0.takeDamage(5);
+	std::cout <<  "------------------\n";
 	User0.beRepaired(3);
 	std::cout <<  "------------------\n";
 	User3 = User0;
 	std::cout <<  "------------------\n";
-	std::cout << "User0  " << User0.getName() << std::endl;
-	std::cout << "User0  " << User0.getHitPoints() << std::endl;
-	std::cout << "User0  " << User0.getEnergyPoints() << std::endl;
-	std::cout << "User0  " << User0.getAttackDamage() << std::endl;
+	std::cout << "Printing User 0 information using getters\n";
+	std::cout << "[User0 name]: " << User0.getName() << std::endl;
+	std::cout << "[ User0  hit points]: " << User0.getHitPoints() << std::endl;
+	std::cout << "[ User0  energy points]: " << User0.getEnergyPoints() << std::endl;
+	std::cout << "[ User0  attack damage]: " << User0.getAttackDamage() << std::endl;
 	return (0);
 }
