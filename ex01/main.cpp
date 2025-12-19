87b3cdea4cae001926bc12f7623144c789adaf50
sboukiou <sboukiou@student.1337.ma>
2025-12-19 19:37:14 +0100

    Adding proper tests in main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,26 +5,38 @@
 int main(void) {
 	std::cout <<  "------------------\n";
 	ScavTrap	User0;
-	std::string u1 = "User1";
 	std::cout <<  "------------------\n";
+	ScavTrap	User1("Dias");
 	std::cout <<  "------------------\n";
 	ScavTrap	User2(User0);
 	std::cout <<  "------------------\n";
-	std::cout <<  "------------------\n";
 
+	/*Setting the class properties*/
+	/*Manually*/
+	std::cout <<  "------------------\n";
+	std::cout << "Setters to setup the Object properties\n";
 	User0.setName("User 0");
 	User0.setHitPoints(10);
 	User0.setEnergyPoints(10);
 	User0.setAttackDamage(10);
+	std::cout <<  "------------------\n";
 
+	std::cout <<  "------------------\n";
 	User0.attack(" Enemy !");
+	std::cout <<  "------------------\n";
 	User0.takeDamage(5);
+	User0.attack(" Enemy !");
+	std::cout << "Attacking with 0 hit  pts left:\n";
+	User0.setHitPoints(0);
+	User0.attack(" Enemy !");
+	std::cout <<  "------------------\n";
 	User0.beRepaired(3);
 	std::cout <<  "------------------\n";
 	std::cout <<  "------------------\n";
-	std::cout << "User0  " << User0.getName() << std::endl;
-	std::cout << "User0  " << User0.getHitPoints() << std::endl;
-	std::cout << "User0  " << User0.getEnergyPoints() << std::endl;
-	std::cout << "User0  " << User0.getAttackDamage() << std::endl;
+	std::cout << "Printing User 0 information using getters\n";
+	std::cout << "[User0 name]: " << User0.getName() << std::endl;
+	std::cout << "[ User0  hit points]: " << User0.getHitPoints() << std::endl;
+	std::cout << "[ User0  energy points]: " << User0.getEnergyPoints() << std::endl;
+	std::cout << "[ User0  attack damage]: " << User0.getAttackDamage() << std::endl;
 	return (0);
 }
